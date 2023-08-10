d83dcb0b9a46485fea2396896aca35f587e30743
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-10 09:52:18 +0200

    Add project files.

---
+++ RestaurantApp/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantApp.Model;
+using RestaurantApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RestaurantApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+        
+        public IConfigurationRoot Configuration { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var cs = Configuration.GetConnectionString("RestaurantDB");
+
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                .AddDbContext<Context>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("RestaurantDB"));
+                })
+                    .AddTransient<MainWindowViewModel>()
+                    .BuildServiceProvider()
+                );
+        }
+    }
+
+    
+}
