ff04fc707e565343b3351dab0407df66e825be81
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-16 09:25:00 +0200

    basic of window done

---
+++ RestaurantApp/Services/DishService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Services
+{
+    public interface IDishService
+    {
+        IEnumerable<Dish> GetAll();
+    }
+    public class DishService : IDishService
+    {
+        public DishService(Context context)
+        {
+            _context = context;
+        }
+        private readonly Context _context;
+
+        public IEnumerable<Dish> GetAll()
+        {
+                return _context.Dishes.Include(x => x.Restaurant);
+        }
+    }
+}
