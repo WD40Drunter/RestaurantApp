8c6e0954e3cc0e2bda44939d058946c717c96c94
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-18 09:47:21 +0200

    adress services

---
+++ RestaurantApp/Services/AdressServices.cs
@@ -0,0 +1,30 @@
+﻿using RestaurantApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Services
+{
+    public interface IAdressServices
+    {
+        int AddAdress(Adress adress);
+    }
+    public class AdressServices : IAdressServices
+    {
+        public AdressServices(Context context)
+        {
+            _context = context;
+        }
+        private readonly Context _context;
+
+        public int AddAdress(Adress adress)
+        {
+            _context.Adresses.Add(adress);
+            _context.SaveChanges();
+
+            return _context.Adresses.FirstOrDefault(adress).AdressId;
+        }
+    }
+}
