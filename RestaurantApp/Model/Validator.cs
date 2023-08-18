83ddef9c5702147f2d0ca1695e010632ad173a5d
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-18 12:17:59 +0200

    validator should work fine

---
+++ RestaurantApp/Model/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RestaurantApp.Model
+{
+    public static class Validator
+    {
+        public static bool IsStringNotNull(string? value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                MessageBox.Show("Upewnij się że wszystko uzupełniłeś");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsHourValid(string value)
+        {
+            if (Convert.ToInt32(value[..2]) >= 24 || Convert.ToInt32(value.Substring(3, 2)) >= 60)
+            {
+                MessageBox.Show("Błędna godzina (godzine 24:00 zapisz jako 00:00)");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsHouseNumberValid(string value)
+        {
+            foreach (var item in value)
+            {
+                if (!char.IsDigit(item) || item != '/')
+                {
+                    MessageBox.Show("Numer domu może zawierać tylko cyfry bądź znak /");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
