69fc158b0745e07e2ad238f3a877d93075d7cb3b
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-24 12:05:51 +0200

    and thats all i think

---
+++ RestaurantApp/Messages/ValuesOfStatusToChangeItInDatabaseMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Messages
+{
+    class ValuesOfStatusToChangeItInDatabaseMessage : ValueChangedMessage<string[]>
+    {
+        public ValuesOfStatusToChangeItInDatabaseMessage(string[] value) : base(value)
+        {
+            
+        }
+    }
+}
