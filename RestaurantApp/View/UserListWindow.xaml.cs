6fc40414fd8c27ba5f26f2da57bf9ba9aa92183a
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-25 11:26:00 +0200

    fundaments layed

---
+++ RestaurantApp/View/UserListWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using RestaurantApp.ViewModel;
+using System.Windows;
+
+namespace RestaurantApp.View
+{
+    /// <summary>
+    /// Interaction logic for UserListWindow.xaml
+    /// </summary>
+    public partial class UserListWindow : Window
+    {
+        public UserListWindow()
+        {
+            InitializeComponent();
+
+            DataContext = Ioc.Default.GetRequiredService<UserListWindowViewModel>();
+        }
+    }
+}
---
+++ RestaurantApp/ViewModel/UserListWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.ViewModel
+{
+    public partial class UserListWindowViewModel : SolutionViewModel
+    {
+        public UserListWindowViewModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+        private readonly IUserService _userService;
+    }
+}
