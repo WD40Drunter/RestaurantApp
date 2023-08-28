8005fbfdc4e07119ff7701ab9701cc5f8e7ffc55
WD40Drunter <kamill.szydlowski@gmail.com>
2023-08-28 14:37:59 +0200

    renamed and modified a bit frontview

---
+++ RestaurantApp/Converters/ObjectToTitleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RestaurantApp.Converters
+{
+    internal class ObjectToTitleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return "Dodaj Restauracje";
+            }
+            return "Edytuj Restauracje";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
